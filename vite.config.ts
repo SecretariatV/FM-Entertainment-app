65d3d08835362cfa8507f64969323e9bdeb1e48f
secretaritatv <oliver.b25.f@gmail.com>
2024-09-21 11:43:51 +0900

    create basic

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    define: {
+      "process.env": env,
+    },
+    server: {
+      port: 3000,
+      host: "0.0.0.0",
+    },
+    resolve: {
+      alias: {
+        "@assets": "/src/assets",
+        "@contexts": "/src/contexts",
+        "@features": "/src/features",
+        "@pages": "/src/pages",
+        "@styles": "/src/styles",
+        "@utils": "/src/utils",
+      },
+    },
+  };
+});
